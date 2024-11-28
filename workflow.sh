d02f3920eb7170e08e94300fc067345df276a096
hwk <85835967+hwkovinci@users.noreply.github.com>
2024-11-28 10:49:13 +0900

    Update

--- workflow.sh
+++ workflow.sh
@@ -5,9 +5,11 @@ CONFIG_FILE="config.ini"
 AVD_NAME=$(awk -F' = ' '/avd_name/ {print $2}' "$CONFIG_FILE")
 APK_PATH=$(awk -F' = ' '/apk_path/ {print $2}' "$CONFIG_FILE")
 AUTOMATION_SCRIPT=$(awk -F' = ' '/automation_script/ {print $2}' "$CONFIG_FILE")
+AVD_ROOT=$(awk -F' = ' '/avd_root/ {print $2}' "$CONFIG_FILE")
+FILE_COPY=$(awk -F' = ' '/file_copy/ {print $2}' "$CONFIG_FILE")
 
 # Start Emulator
-python scripts/emulator.py start --avd-name "$AVD_NAME"
+python scripts/emulator.py start --avd-root "$AVD_ROOT" --avd-name "$AVD_NAME" --file-copy "$FILE_COPY"   
 
 # Install APK
 python scripts/apk_installer.py --apk-path "$APK_PATH"
